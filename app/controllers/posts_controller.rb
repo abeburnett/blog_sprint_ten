1516ae322c87c1686aca559a56977d322e1b269a
Abraham R Burnett <abeburnett@gmail.com>
2014-09-23 17:46:31 -0700

    delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,4 +34,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
 end
